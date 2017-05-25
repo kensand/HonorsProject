7f4648a8a6222a787325ee6e2f010b7a0cc64140
Kenny Sanders <kennysandbum@gmail.com>
2017-05-25 18:50:22 -0400

    more work

--- TextToInts.py
+++ TextToInts.py
@@ -4,18 +4,55 @@ import time
 
 import Database
 
-# The purpose of this file is to convert the tweet_text given in the formatted tweet table with columns id, tweet_text and convert it into an array of integers representing the sequence of tokens
+# The purpose of this file is to convert the tweet_text given fin the formatted tweet table with columns id, tweet_text and convert it into an array of integers representing the sequence of tokens
 
 
 # parse arguements
 # note, default database settings can be found in the database file.
-parser = argparse.ArgumentParser(prog='FormatTweets',
-                                 usage='python FormatTweets -i tweets -o formatted tweets -u user -p -h localhost -d postgres',
+parser = argparse.ArgumentParser(prog='TextToInts',
+                                 usage='python TextToInts -i formatted_tweets -k dictionary -o output_table -d dbname -c host -u user -p',
                                  description="\n\n This program TRUNCATES the output table 'formatted_tweets' at the beginning by default.")
-parser.add_argument('-i', '--input', action='store', dest='input', default=Database.tweets['table_name'],
+
+
+
+parser.add_argument('-i', '--input', action='store', dest='input', default=Database.formatted_tweets['table_name'],
+                    help='')
+parser.add_argument('-iic', '--input_id_column', action='store', dest='in_id_column',
+                    default=Database.formatted_tweets['tweet_id_column'], help='The name of the input column holding the tweet ids.')
+parser.add_argument('-itc', '--input_tokens_column', action='store', dest='in_tokens_column',
+                    default=Database.formatted_tweets['tokens_column'], help='The name of the input column holding the tweet tokens.')
+
+
+
+#dictionary options
+parser.add_argument('-k', '--dictionary', action='store', dest='dict', default=Database.dictionary['table_name'],
                     help='The name of the input table, which should have columns "id" and "text", where id is a big int representing the tweet id, and text is a string of some sort.')
-parser.add_argument('-o', '--output', action='store', dest='output', default=Database.formatted_tweets['table_name'],
+
+parser.add_argument('-kic', '--dictionary_word_id_column', action='store', dest='dict_word_id_column', default=Database.dictionary['word_id_column'],
+                    help='')
+parser.add_argument('-kwc', '--dictionary_word_column', action='store', dest='dict_word_column', default=Database.dictionary['word_column'],
+                    help='')
+
+#output options
+parser.add_argument('-o', '--output', action='store', dest='output', default=Database.int_tweets['table_name'],
                     help='The name of the output table, which will have columns "id", and "tokens", where id will be the same as the input id, and tokens will be a tokenized and formatted array of strings.')
+parser.add_argument('-oic', '--output_id_column', action='store', dest='out_id_column',
+                    default=Database.formatted_tweets['tweet_id_column'],
+                    help='The name of the output column to which the tweet ids will be written.')
+parser.add_argument('-otc', '--output_arr_column', action='store', dest='out_int_array_column',
+                    default=Database.int_tweets['int_array_column'],
+                    help='The name of the output column to which the integer arrays will be written.')
+
+
+#utility options
+parser.add_argument('-w', '--where', action='store', dest='where', default=False,
+                    help='An optional WHERE filter for the input SELECT call. This allows you to add filters to the input data. You should write only the contents of the WHERE clause as it would be written in PSQL')
+parser.add_argument('-a', '--append', action='store_const', const=True, dest='append', default=False,
+                    help='Using this flag will cause the output to be appended to the table as opposed to truncating the table.')
+
+
+
+#database options
 parser.add_argument('-u', '--user', action='store', dest='user', default=Database.User,
                     help='The user to login to the database as.')
 parser.add_argument('-p', '--password', action='store_const', const=True, dest='password', default=False,
@@ -24,20 +61,8 @@ parser.add_argument('-c', '--host', action='store', dest='host', default=Databas
                     help='The host of the database to be accessed.')
 parser.add_argument('-d', '--dbname', action='store', dest='dbname', default=Database.Dbname,
                     help='The database name at the host.')
-parser.add_argument('-a', '--append', action='store_const', const=True, dest='append', default=False,
-                    help='Using this flag will cause the output to be appended to the table as opposed to truncating the table.')
-parser.add_argument('-iic', '--input_id_column', action='store', dest='in_id_column',
-                    default=Database.tweets['id_column'], help='The name of the input column holding the tweet ids.')
-parser.add_argument('-itc', '--input_text_column', action='store', dest='in_text_column',
-                    default=Database.tweets['text_column'], help='The name of the input column holding the tweet ids.')
-parser.add_argument('-oic', '--output_id_column', action='store', dest='out_id_column',
-                    default=Database.formatted_tweets['id_column'],
-                    help='The name of the output column to which the tweet ids will be written.')
-parser.add_argument('-otc', '--output_token_column', action='store', dest='out_token_column',
-                    default=Database.formatted_tweets['tokens_column'],
-                    help='The name of the output column to which the tokens will be written.')
-parser.add_argument('-w', '--where', action='store', dest='where', default=False,
-                    help='An optional WHERE filter for the input SELECT call. This allows you to add filters to the input data. You should write only the contents of the WHERE clause as it would be written in PSQL')
+
+
 
 # create the connection
 args = parser.parse_args()
@@ -48,6 +73,10 @@ if args.password:
 else:
     conn = Database.get_Conn(user=args.user, password=Database.Password, host=args.host, dbname=args.dbname)
 
+#get the dictionary
+dictionary = Database.get_dictionary(cursor=conn.cursor(), table=args.dict, word_column=args.dict_word_column, word_id_column=args.dict_word_id_column  )
+
+
 incur = conn.cursor()
 outcur = conn.cursor()
 if args.append == False:
@@ -55,7 +84,7 @@ if args.append == False:
     outcur.execute("""COMMIT""")
     print "Truncated " + args.output
 
-select = "SELECT " + args.in_id_column + ", " + args.in_text_column + " FROM " + args.input
+select = "SELECT " + args.in_id_column + ", " + args.in_tokens_column + " FROM " + args.input
 if args.where != False:
     select += ' WHERE ' + args.where
 print "Executing: "
@@ -67,9 +96,16 @@ start = time.localtime()
 print 'Started at: ' + time.strftime("%b %d %Y %H:%M:%S", start)
 for row in incur:
 
-    id, text = row[0], row[1]
+    id, tokens = row[0], row[1]
+    arr = []
+    for token in tokens:
+        if token in dictionary:
+            arr.append(dictionary[token])
+        else:
+            arr.append(dictionary['/*UNKOWN*/'])
+
     insert = 'INSERT INTO ' + args.output + ' (' + args.out_id_column + ', ' + args.out_token_column + ') VALUES (%s, %s)'
-    outcur.execute(insert, [id, tokens])
+    outcur.execute(insert, [id, arr])
     if incur.rownumber % 1000 == 1:  # int(incur.rowcount / 100) == 0:
         fin = ((time.mktime(time.localtime()) - time.mktime(start)) / incur.rownumber) * incur.rowcount
         fin += time.mktime(start)
