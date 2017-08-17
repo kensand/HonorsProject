3e79ded2b8ee9943aa1d20cb4f02a125e5ed6587
Kenny Sanders <kennysandbum@gmail.com>
2017-08-17 13:37:17 -0400

    stuff?

--- Data_Processing_Scripts/RunSpecific.py
+++ Data_Processing_Scripts/RunSpecific.py
@@ -25,9 +25,9 @@ exit(0)
 
 runs = {'abortionweek1': """id IN (SELECT id from tweets WHERE created_at >= '2016-06-24' and created_at < '2016-07-01' and issue='abortion')""", 'abortionweek2': """id IN (SELECT id from tweets WHERE created_at >= '2016-07-01' and created_at < '2016-07-08' and issue='abortion')""", 'abortionweek3': """id IN (SELECT id from tweets WHERE created_at >= '2016-07-08' and created_at < '2016-07-15' and issue='abortion')""", 'abortionweek4': """id IN (SELECT id from tweets WHERE created_at >= '2016-07-15' and created_at < '2016-07-22' and issue='abortion')""", 'abortionweek5': """id IN (SELECT id from tweets WHERE created_at >= '2016-07-22' and created_at < '2016-07-29' and issue='abortion')""", 'abortionweek6': """id IN (SELECT id from tweets WHERE created_at >= '2016-07-29' and created_at < '2016-08-05' and issue='abortion')""", 'abortionweek7': """id IN (SELECT id from tweets WHERE created_at >= '2016-08-05' and created_at < '2016-08-12' and issue='abortion')""", 'abortionweek8': """id IN (SELECT id from tweets WHERE created_at >= '2016-08-12' and created_at < '2016-08-19' and issue='abortion')""", 'abortionweek9': """id IN (SELECT id from tweets WHERE created_at >= '2016-08-19' and created_at < '2016-08-26' and issue='abortion')""", 'abortionweek10': """id IN (SELECT id from tweets WHERE created_at >= '2016-08-26' and created_at < '2016-09-02' and issue='abortion')"""}#, 'abortionall': """id IN (SELECT id from tweets WHERE issue='abortion')"""}
 #runs= { 'abortionweek2': """id IN (SELECT id from tweets WHERE created_at >= '2016-07-01' and created_at < '2016-07-08' and issue='abortion')""", 'abortionweek3': """id IN (SELECT id from tweets WHERE created_at >= '2016-07-08' and created_at < '2016-07-15' and issue='abortion')""",}
-runs = {'abortionall':'id IN (SELECT id from public.test)'}
+runs = {'abortiontrain':'id IN (SELECT id from public.train)'}
 
-FormatTweets.format_tweets(append=False, where=""" issue ='abortion'""", commit=True)
+#FormatTweets.format_tweets(append=False, where=""" issue ='abortion'""", commit=True)
 
 #runs = {'abortionall': """id IN (SELECT id from tweets WHERE issue='abortion')"""}
 for schema, where in runs.items():
